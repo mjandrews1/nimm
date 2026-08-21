a3fdd22273577feb2fd6ee9937d0aa60159c7901
mjandrews1 <mjandrews@zohomail.com>
2026-08-20 19:33:16 -0500

    Bump version to nimm 0.1.2

--- main.nim
+++ main.nim
@@ -8,7 +8,7 @@
 #   nimm --repl                                # Interactive REPL
 #   nimm -m strict -x 'SET X=1'               # Strict mode
 
-const Version* = "0.1.1"
+const Version* = "0.1.2"
 
 import os
 import strutils
--- special_vars.nim
+++ special_vars.nim
@@ -214,7 +214,7 @@ proc setZtrap(val: string) =
   ztrap = val
 
 proc getZversion(): string =
-  return "nimm 0.1.1"
+  return "nimm 0.1.2"
 
 proc registerAllSpecialVars*(g: var Globals) =
   ## Register all special variables with the globals system
