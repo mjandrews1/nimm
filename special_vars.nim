d7f527655188c175fb0b1251d7d955adcfd185a4
mjandrews1 <mjandrews@zohomail.com>
2026-08-22 19:35:39 -0500

    Bump version to nimm 0.1.5

--- special_vars.nim
+++ special_vars.nim
@@ -251,7 +251,7 @@ proc setZtrap(val: string) =
   ztrap = val
 
 proc getZversion(): string =
-  return "nimm 0.1.4"
+  return "nimm 0.1.5"
 
 proc registerAllSpecialVars*(g: var Globals) =
   ## Register all special variables with the globals system
